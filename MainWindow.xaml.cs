888e6a795586b12fdabf1abf7bd3373ba68ccd2a
alumno <alumno@A302PC36>
2022-09-30 08:48:06 +0200

    Agregar archivos de proyecto.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tema2_CaracteresLimitados
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void cuadro_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            contador.Text = cuadro.Text.Length.ToString() + "/140";
+
+            if (cuadro.Text.Length == 140)
+                cuadro.IsReadOnly = true;
+        }
+    }
+}
